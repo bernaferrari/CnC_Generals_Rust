53f245f4ea497b38f9ce5a47994d387742aed124
Bernardo Ferrari <bernaferrari2@gmail.com>
2026-05-09 15:26:54 -0300

    Match animated particle bone save block

--- GeneralsRust/Code/GameEngine/GameLogic/src/object/update/animated_particle_sys_bone_client_update.rs
+++ GeneralsRust/Code/GameEngine/GameLogic/src/object/update/animated_particle_sys_bone_client_update.rs
@@ -58,7 +58,6 @@ impl AnimatedParticleSysBoneClientUpdateModule {
 }
 
 impl Module for AnimatedParticleSysBoneClientUpdateModule {
-
     fn get_module_name_key(&self) -> NameKeyType {
         self.module_name_key
     }
@@ -77,11 +76,51 @@ impl Snapshotable for AnimatedParticleSysBoneClientUpdateModule {
         Ok(())
     }
 
-    fn xfer(&mut self, _xfer: &mut dyn Xfer) -> Result<(), String> {
+    fn xfer(&mut self, xfer: &mut dyn Xfer) -> Result<(), String> {
+        const CURRENT_VERSION: u8 = 1;
+        let mut version = CURRENT_VERSION;
+        xfer.xfer_version(&mut version, CURRENT_VERSION)
+            .map_err(|e| format!("{:?}", e))?;
         Ok(())
     }
 
     fn load_post_process(&mut self) -> Result<(), String> {
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use game_engine::common::system::xfer_load::XferLoad;
+    use game_engine::common::system::xfer_save::XferSave;
+    use game_engine::common::thing::module::BaseModuleData;
+    use std::io::Cursor;
+
+    #[test]
+    fn animated_particle_sys_bone_xfer_writes_cpp_version_only_block() {
+        let module_data = Arc::new(BaseModuleData::new());
+        let mut saved = AnimatedParticleSysBoneClientUpdateModule::new(11, module_data.clone(), 22);
+        saved.life = 1234;
+
+        let mut bytes = Vec::new();
+        {
+            let cursor = Cursor::new(&mut bytes);
+            let mut save = XferSave::new(cursor, 1);
+            save.open("animated_particle_sys_bone").unwrap();
+            saved.xfer(&mut save).unwrap();
+            save.close().unwrap();
+        }
+
+        let mut loaded = AnimatedParticleSysBoneClientUpdateModule::new(11, module_data, 22);
+        loaded.life = 77;
+        {
+            let mut load = XferLoad::new(Cursor::new(bytes), 1);
+            load.open("animated_particle_sys_bone").unwrap();
+            loaded.xfer(&mut load).unwrap();
+            load.close().unwrap();
+        }
+
+        assert_eq!(loaded.life, 77);
     }
 }
