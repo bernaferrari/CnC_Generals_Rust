af7628cb1cb953cf50a90570e41cdb578174cff9
Bernardo Ferrari <bernaferrari2@gmail.com>
2026-05-16 22:28:03 -0300

    Make ModelState conversion explicit

--- GeneralsRust/Code/GameEngine/Common/src/model_state.rs
+++ GeneralsRust/Code/GameEngine/Common/src/model_state.rs
@@ -75,7 +75,7 @@ pub enum ModelConditionFlagType {
     RadarExtending = 58,
     RadarUpgraded = 59,
     Panicking = 60,
-    Aframe = 61,
+    Aflame = 61,
     Smoldering = 62,
     Burned = 63,
     Docking = 64,
@@ -145,9 +145,127 @@ impl ModelConditionFlagType {
     pub fn from_i32(v: i32) -> Self {
         match v {
             -1 => Self::Invalid,
-            0..=116 => unsafe { std::mem::transmute(v) },
+            0 => Self::Toppled,
+            1 => Self::FrontCrushed,
+            2 => Self::BackCrushed,
+            3 => Self::Damaged,
+            4 => Self::ReallyDamaged,
+            5 => Self::Rubble,
+            6 => Self::SpecialDamaged,
+            7 => Self::Night,
+            8 => Self::Snow,
+            9 => Self::Parachuting,
+            10 => Self::Garrisoned,
+            11 => Self::EnemyNear,
+            12 => Self::WeaponsetVeteran,
+            13 => Self::WeaponsetElite,
+            14 => Self::WeaponsetHero,
+            15 => Self::WeaponsetCrateupgradeOne,
+            16 => Self::WeaponsetCrateupgradeTwo,
+            17 => Self::WeaponsetPlayerUpgrade,
+            18 => Self::Door1Opening,
+            19 => Self::Door1Closing,
+            20 => Self::Door1WaitingOpen,
+            21 => Self::Door1WaitingToClose,
+            22 => Self::Door2Opening,
+            23 => Self::Door2Closing,
+            24 => Self::Door2WaitingOpen,
+            25 => Self::Door2WaitingToClose,
+            26 => Self::Door3Opening,
+            27 => Self::Door3Closing,
+            28 => Self::Door3WaitingOpen,
+            29 => Self::Door3WaitingToClose,
+            30 => Self::Door4Opening,
+            31 => Self::Door4Closing,
+            32 => Self::Door4WaitingOpen,
+            33 => Self::Door4WaitingToClose,
+            34 => Self::Attacking,
+            35 => Self::PreattackA,
+            36 => Self::FiringA,
+            37 => Self::BetweenFiringShotsA,
+            38 => Self::ReloadingA,
+            39 => Self::PreattackB,
+            40 => Self::FiringB,
+            41 => Self::BetweenFiringShotsB,
+            42 => Self::ReloadingB,
+            43 => Self::PreattackC,
+            44 => Self::FiringC,
+            45 => Self::BetweenFiringShotsC,
+            46 => Self::ReloadingC,
+            47 => Self::TurretRotate,
+            48 => Self::PostCollapse,
+            49 => Self::Moving,
+            50 => Self::Dying,
+            51 => Self::AwaitingConstruction,
+            52 => Self::PartiallyConstructed,
+            53 => Self::ActivelyBeingConstructed,
+            54 => Self::Prone,
+            55 => Self::Freefall,
+            56 => Self::ActivelyConstructing,
+            57 => Self::ConstructionComplete,
+            58 => Self::RadarExtending,
+            59 => Self::RadarUpgraded,
+            60 => Self::Panicking,
+            61 => Self::Aflame,
+            62 => Self::Smoldering,
+            63 => Self::Burned,
+            64 => Self::Docking,
+            65 => Self::DockingBeginning,
+            66 => Self::DockingActive,
+            67 => Self::DockingEnding,
+            68 => Self::Carrying,
+            69 => Self::Flooded,
+            70 => Self::Loaded,
+            71 => Self::JetAfterburner,
+            72 => Self::JetExhaust,
+            73 => Self::Packing,
+            74 => Self::Unpacking,
+            75 => Self::Deployed,
+            76 => Self::OverWater,
+            77 => Self::PowerPlantUpgraded,
+            78 => Self::Climbing,
+            79 => Self::Sold,
+            80 => Self::Rappelling,
+            81 => Self::Armed,
+            82 => Self::PowerPlantUpgrading,
+            83 => Self::SpecialCheering,
+            84 => Self::ContinuousFireSlow,
+            85 => Self::ContinuousFireMean,
+            86 => Self::ContinuousFireFast,
+            87 => Self::RaisingFlag,
+            88 => Self::Captured,
+            89 => Self::ExplodedFlailing,
+            90 => Self::ExplodedBouncing,
+            91 => Self::Splatted,
+            92 => Self::UsingWeaponA,
+            93 => Self::UsingWeaponB,
+            94 => Self::UsingWeaponC,
+            95 => Self::Preorder,
+            96 => Self::CenterToLeft,
+            97 => Self::LeftToCenter,
+            98 => Self::CenterToRight,
+            99 => Self::RightToCenter,
+            100 => Self::Rider1,
+            101 => Self::Rider2,
+            102 => Self::Rider3,
+            103 => Self::Rider4,
+            104 => Self::Rider5,
+            105 => Self::Rider6,
+            106 => Self::Rider7,
+            107 => Self::Rider8,
+            108 => Self::StunnedFlailing,
+            109 => Self::Stunned,
+            110 => Self::SecondLife,
+            111 => Self::Jammed,
+            112 => Self::ArmorsetCrateupgradeOne,
+            113 => Self::ArmorsetCrateupgradeTwo,
+            114 => Self::User1,
+            115 => Self::User2,
+            116 => Self::Disguised,
             _ => {
-                log::warn!("ModelConditionFlagType::from_i32({v}) out of range, defaulting to Invalid");
+                log::warn!(
+                    "ModelConditionFlagType::from_i32({v}) out of range, defaulting to Invalid"
+                );
                 Self::Invalid
             }
         }
@@ -155,5 +273,36 @@ impl ModelConditionFlagType {
 
     pub fn to_index(self) -> usize {
         (self as i32) as usize
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn model_condition_saved_values_match_cpp_order() {
+        assert_eq!(ModelConditionFlagType::Invalid as i32, -1);
+        assert_eq!(ModelConditionFlagType::Toppled as i32, 0);
+        assert_eq!(ModelConditionFlagType::Aflame as i32, 61);
+        assert_eq!(ModelConditionFlagType::Disguised as i32, 116);
+        assert_eq!(ModelConditionFlagType::Count as i32, 117);
+
+        assert_eq!(
+            ModelConditionFlagType::from_i32(-1),
+            ModelConditionFlagType::Invalid
+        );
+        assert_eq!(
+            ModelConditionFlagType::from_i32(61),
+            ModelConditionFlagType::Aflame
+        );
+        assert_eq!(
+            ModelConditionFlagType::from_i32(116),
+            ModelConditionFlagType::Disguised
+        );
+        assert_eq!(
+            ModelConditionFlagType::from_i32(117),
+            ModelConditionFlagType::Invalid
+        );
     }
 }
