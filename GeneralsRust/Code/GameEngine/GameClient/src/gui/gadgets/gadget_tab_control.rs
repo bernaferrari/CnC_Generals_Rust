ab15874fd250dd1fce4b6dabf2a0fbe07ff5f576
Bernardo Ferrari <bernaferrari2@gmail.com>
2026-06-13 20:14:30 -0300

    Emit tab control render commands

--- GeneralsRust/Code/GameEngine/GameClient/src/gui/gadgets/gadget_tab_control.rs
+++ GeneralsRust/Code/GameEngine/GameClient/src/gui/gadgets/gadget_tab_control.rs
@@ -1,3 +1,3 @@
 //! C++ parity wrapper for GadgetTabControl.cpp
 
-pub use super::tabcontrol::{Tab, TabCallback, TabControl};
+pub use super::tabcontrol::{Tab, TabCallback, TabControl, TabControlRenderCommand};
