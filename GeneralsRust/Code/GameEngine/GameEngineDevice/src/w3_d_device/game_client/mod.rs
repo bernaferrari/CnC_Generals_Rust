061d8d51f27d286fc551b083a27f50c3e363f6a2
Bernardo Ferrari <bernaferrari2@gmail.com>
2026-06-13 08:18:31 -0300

    Port W3D prop buffer

--- GeneralsRust/Code/GameEngine/GameEngineDevice/src/w3_d_device/game_client/mod.rs
+++ GeneralsRust/Code/GameEngine/GameEngineDevice/src/w3_d_device/game_client/mod.rs
@@ -1,7 +1,8 @@
 // W3D device game client modules (C++: W3DDevice/GameClient)
 
 pub mod w3_d_bib_buffer;
-pub mod w3_d_dynamic_light;
 pub mod w3_d_debug_icons;
+pub mod w3_d_dynamic_light;
 pub mod w3_d_file_system;
 pub mod w3_d_poly;
+pub mod w3_d_prop_buffer;
