fdbbb57728f0c152e19ad3767568570fa872b601
Bernardo Ferrari <bernaferrari2@gmail.com>
2026-06-13 07:41:43 -0300

    Port W3D common factories and camera shake parity

--- GeneralsRust/Code/GameEngine/GameEngineDevice/src/w3_d_device/common/mod.rs
+++ GeneralsRust/Code/GameEngine/GameEngineDevice/src/w3_d_device/common/mod.rs
@@ -1,5 +1,7 @@
 // W3D device common modules (C++: W3DDevice/Common)
 
 pub mod system;
+/// W3D-specific Thing and Module factory adapters.
+pub mod thing;
 pub mod w3_d_convert;
 pub mod w3_d_function_lexicon;
